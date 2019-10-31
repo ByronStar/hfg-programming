d55053661482deb3347f0cad119f21c98b785b19
ByronStar <benno@fam-staebler.de>
2019-10-31 07:24:51 +0100

    Create certificates at runtime

--- game/cert/certgen.sh
+++ game/cert/certgen.sh
@@ -8,19 +8,19 @@ openssl req -utf8 -config progsp.ca-hfg-gmuend.de.conf -x509 -new -nodes -key ro
 
 # Each Server
 # Create key
-openssl genrsa -out progsp.hfg-gmuend.de.key 2048
+#openssl genrsa -out progsp.hfg-gmuend.de.key 2048
 # signing (csr)
-openssl req -utf8 -config progsp.hfg-gmuend.de.conf -new -key progsp.hfg-gmuend.de.key -out progsp.hfg-gmuend.de.csr
+#openssl req -utf8 -config progsp.hfg-gmuend.de.conf -new -key progsp.hfg-gmuend.de.key -out progsp.hfg-gmuend.de.csr
 # Verify csr
-openssl req -in progsp.hfg-gmuend.de.csr -noout -text
+#openssl req -in progsp.hfg-gmuend.de.csr -noout -text
 # Create certificate
-openssl x509 -req -in progsp.hfg-gmuend.de.csr -CA rootCA.crt -CAkey rootCA.key -CAcreateserial -out progsp.hfg-gmuend.de.pem -days 365 -sha256
+#openssl x509 -req -in progsp.hfg-gmuend.de.csr -CA rootCA.crt -CAkey rootCA.key -CAcreateserial -out progsp.hfg-gmuend.de.pem -days 365 -sha256
 # Verify certificate
-openssl x509 -in progsp.hfg-gmuend.de.pem -text -noout
+#openssl x509 -in progsp.hfg-gmuend.de.pem -text -noout
 
 mv rootCA.crt ../server
-mv progsp.hfg-gmuend.de.key ../server
-mv progsp.hfg-gmuend.de.pem ../server
+#mv progsp.hfg-gmuend.de.key ../server
+#mv progsp.hfg-gmuend.de.pem ../server
 
 #openssl req -utf8 -config progsp.hfg-gmuend.de.conf -new -sha256 -newkey rsa:2048 -nodes -keyout progsp.hfg-gmuend.de.key -x509 -days 365 -out progsp.hfg-gmuend.de.pem
 
--- game/server/gameserver.sh
+++ game/server/gameserver.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env sh
+# Create key
+openssl genrsa -out progsp.hfg-gmuend.de.key 2048
+# signing (csr)
+openssl req -utf8 -config progsp.hfg-gmuend.de.conf -new -key progsp.hfg-gmuend.de.key -out progsp.hfg-gmuend.de.csr
+# Create certificate
+openssl x509 -req -in progsp.hfg-gmuend.de.csr -CA rootCA.crt -CAkey rootCA.key -CAcreateserial -out progsp.hfg-gmuend.de.pem -days 365 -sha256
+
 rc=0
 #dir=~/bitbucket/hfg-programming/
 dir=~/github/hfg-programming/
